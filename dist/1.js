f41b13ec46760f13b78f0105ee29259d8a57de3b
Muhammad Hameed <muhammadhameed105@gmail.com>
2024-02-05 17:36:32 +0500

    default parameters

--- dist/1.js
+++ dist/1.js
@@ -1,32 +1,39 @@
 "use strict";
 // var hoisting
 // has functional scope
-{
-    var greet = 'Hello';
-}
-console.log(greet);
+// {
+//   var greet = 'Hello';
+// }
+// console.log(greet);
 // let has block scope
 // is not hoisted
-{
-    let salutation = 'Mrs.';
-}
+// {
+//   let salutation = 'Mrs.';
+// }
 // throws error
 // console.log(salutation);
 // for loop variable being passed as a reference
-for (var i = 0; i < 5; i++) {
-    setTimeout(function () {
-        console.log(i);
-    }, 1000);
-}
+// for (var i = 0; i < 5; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, 1000);
+// }
 // using let for each callback function to
 // maintain its own version of i
-for (let i = 0; i < 5; i++) {
-    setTimeout(function () {
-        console.log(i);
-    }, 1000);
-}
+// for (let i = 0; i < 5; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, 1000);
+// }
 // arrow function
-const arrowFunc = () => {
-    return;
-};
+// arrow function don't have a 'this' context
+// const arrowFunc = () => {
+//   return;
+// };
+// function with default parameters
+function sum(a = 10, b = a * 0.1) {
+    console.log(arguments.length);
+    return a + b;
+}
+// sum(20, 20);
 //# sourceMappingURL=1.js.map
