2533fe6e2240db34741102686d27473f2bfd6a51
Muhammad Hameed <muhammadhameed105@gmail.com>
2024-02-05 17:12:00 +0500

    var and for loop

--- dist/1.js
+++ dist/1.js
@@ -12,4 +12,17 @@ console.log(greet);
 }
 // throws error
 // console.log(salutation);
+// for loop variable being passed as a reference
+for (var i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 1000);
+}
+// using let for each callback function to
+// maintain its own version of i
+for (let i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 1000);
+}
 //# sourceMappingURL=1.js.map
